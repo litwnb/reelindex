aa90484b011706943ab5470071f3323c9be44c8e
Kostiantyn Mamaisur <kmamaisur@gmail.com>
2025-07-08 17:49:21 +0300

    service imports optimization

--- src/main/java/com/litwnb/reelindex/service/MovieService.java
+++ src/main/java/com/litwnb/reelindex/service/MovieService.java
@@ -4,7 +4,6 @@ import com.litwnb.reelindex.model.Genre;
 import com.litwnb.reelindex.model.MovieDTO;
 import org.springframework.data.domain.Page;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface MovieService {
